efee342893be25f6296e37c04fdc30c25d22b872
alvaroanavarro1 <38303261+alvaroanavarro1@users.noreply.github.com>
2020-04-01 15:06:30 -0400

    fix Preprocessing_data script, and first linear regression (Salary)

---
+++ Simple_Linear_Regresion/Simple_Linear_Regression.py
@@ -0,0 +1,34 @@
+#Regresión linear simple
+
+#Pre procesado de datos
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+#Importar el dataset
+dataset = pd.read_csv('Salary_Data.csv')
+
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+#Dividir el dataset entrenamiento/testing
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 1/3, random_state = 0)
+
+#Crear modelo de regresión simple con el conjunto de entrenamiento
+regression = LinearRegression()
+regression.fit(x_train, y_train)
+
+#Predecir el conjunto de prueba
+y_pred = regression.predict(x_test)
+
+#Visualización de datos de entrenamiento
+plt.scatter(x_train, y_train, color = "red", label = "Train data")
+plt.scatter(x_test, y_test, color = "green", label = "Test data")
+plt.plot(x_train, regression.predict(x_train), color = "blue", label = "Regression line")
+plt.title("Sueldo vs Años de experiencia (Conjunto de entrenamiento)")
+plt.xlabel("Años de experiencia")
+plt.ylabel("Sueldo ($)")
+plt.legend()
+plt.show()
