ccb4491330fd5cb98412fbab23a172565b595b02
alvaroanavarro1 <38303261+alvaroanavarro1@users.noreply.github.com>
2020-04-12 16:41:06 -0400

    KNN and SVM algorithms with graphics

---
+++ Classification/K-NN/K-NN.py
@@ -0,0 +1,70 @@
+#K-Nearest Neighbors
+
+#Pre procesado de datos
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+
+#Importar el dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+x = dataset.iloc[:, 2:4].values
+y = dataset.iloc[:, 4].values
+
+#Dividir el dataset entrenamiento/testing
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+
+#Escalado de variables
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+#Ajustar el modelo de KNN en el conjunto de entrenamiento
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(x_train, y_train)
+
+#Predicción de los resultados con el conjunto de testing
+y_pred =  classifier.predict(x_test)
+
+#Elaborar una matriz de confusion
+cm = confusion_matrix(y_test, y_pred)
+
+#Representacion grafica de los resultados del algoritmo en el conjunto de entrenamiento
+x_set, y_set = x_train, y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN (Conjunto de Entrenamiento)')
+plt.xlabel('Edad')
+plt.ylabel('Sueldo Estimado')
+plt.legend()
+plt.show()
+
+# Representación gráfica de los resultados del algoritmo en el Conjunto de Testing
+x_set, y_set = x_test, y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN (Conjunto de Test)')
+plt.xlabel('Edad')
+plt.ylabel('Sueldo Estimado')
+plt.legend()
+plt.show()
