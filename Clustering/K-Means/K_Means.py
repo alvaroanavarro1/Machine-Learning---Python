5cb1b7b632218cf0aa858e5ede5b2655bd9479c0
alvaroanavarro1 <38303261+alvaroanavarro1@users.noreply.github.com>
2020-04-15 14:35:58 -0400

    Clustering folder and K-Means algorithm

---
+++ Clustering/K-Means/K_Means.py
@@ -0,0 +1,45 @@
+#K-Means
+
+#Pre procesado de datos
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix
+
+#Importar el dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+
+x = dataset.iloc[:, 3:5].values
+
+#Metodo del codo para conocer el numero optimo de clusters
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = "k-means++", max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1,11), wcss)
+plt.title("Método del codo")
+plt.xlabel("Número de Clusters")
+plt.ylabel("WCSS(k)")
+plt.show()
+
+# Aplicar el método de k-means para segmentar el data set
+kmeans = KMeans(n_clusters = 5, init="k-means++", max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(x)
+
+# Visualización de los clusters
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s = 100, c = "red", label = "Cautos")
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s = 100, c = "blue", label = "Estandard")
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s = 100, c = "green", label = "Objetivo")
+plt.scatter(x[y_kmeans == 3, 0], x[y_kmeans == 3, 1], s = 100, c = "cyan", label = "Descuidados")
+plt.scatter(x[y_kmeans == 4, 0], x[y_kmeans == 4, 1], s = 100, c = "magenta", label = "Conservadores")
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s = 300, c = "yellow", label = "Baricentros")
+plt.title("Cluster de clientes")
+plt.xlabel("Ingresos anuales (en miles de $)")
+plt.ylabel("Puntuación de Gastos (1-100)")
+plt.legend()
+plt.show()
